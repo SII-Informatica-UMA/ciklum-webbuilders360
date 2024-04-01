3dfccdd1eaf2f9c7ec7411f7db58b1edb1eafdf7
florrosa <125082598+monica-l-p@users.noreply.github.com>
2024-04-01 11:56:17 +0200

    Autocompletado

--- project/my-app/src/app/enviarMensaje/enviarMensaje.component.ts
+++ project/my-app/src/app/enviarMensaje/enviarMensaje.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { FormControl, FormsModule, Validators, FormGroup } from '@angular/forms';
-import { Mensaje } from '../mensaje';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Destinatario } from '../destinatario';
 import { MensajeService } from '../mensaje.service';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
     selector: 'enviarMensaje',
     standalone: true,
-    imports: [FormsModule, MatAutocompleteModule],
+    imports: [MatInputModule, ReactiveFormsModule, AsyncPipe, FormsModule, MatFormFieldModule, MatAutocompleteModule],
     templateUrl: './enviarMensaje.component.html',
     styleUrls: ['./enviarMensaje.component.css']
 })
@@ -21,10 +23,23 @@ export class EnviarMensaje {
     copiaOculta: Destinatario[] = [];
     nombresDestinatarios: readonly string[];
 
-    fromGroup = new FormGroup({control: new FormControl()});
+    @ViewChild('inputDestinatario') inputDestinatario!: ElementRef<HTMLInputElement>;
+    myControlDestinatario = new FormControl('');
+    filteredOptionsDestinatario: string[];
+
+    @ViewChild('inputCopia') inputCopia!: ElementRef<HTMLInputElement>;
+    myControlCopia = new FormControl('');
+    filteredOptionsCopia: string[];
+
+    @ViewChild('inputCopiaOculta') inputCopiaOculta!: ElementRef<HTMLInputElement>;
+    myControlCopiaOculta = new FormControl('');
+    filteredOptionsCopiaOculta: string[];
 
     public constructor(private mensajeService: MensajeService) {
         this.nombresDestinatarios = mensajeService.getNombresDestinatarios();
+        this.filteredOptionsDestinatario = this.nombresDestinatarios.slice();
+        this.filteredOptionsCopia = this.nombresDestinatarios.slice();
+        this.filteredOptionsCopiaOculta = this.nombresDestinatarios.slice();
     }
 
     public async enviarMensaje() {
@@ -33,5 +48,20 @@ export class EnviarMensaje {
             copia: this.mensajeService.destinatarios2DestinatariosDTO(this.copia),
             copiaOculta: this.mensajeService.destinatarios2DestinatariosDTO(this.copiaOculta),
             contenido: this.contenido});
+    }
+
+    public filterDestinatario(): void {
+        const filterValue = this.inputDestinatario.nativeElement.value.toLowerCase();
+        this.filteredOptionsDestinatario = this.nombresDestinatarios.filter(o => o.toLowerCase().includes(filterValue));
+    }
+
+    public filterCopia(): void {
+        const filterValue = this.inputCopia.nativeElement.value.toLowerCase();
+        this.filteredOptionsCopia = this.nombresDestinatarios.filter(o => o.toLowerCase().includes(filterValue));
+    }
+
+    public filterCopiaOculta(): void {
+        const filterValue = this.inputCopiaOculta.nativeElement.value.toLowerCase();
+        this.filteredOptionsCopiaOculta = this.nombresDestinatarios.filter(o => o.toLowerCase().includes(filterValue));
     }
 }
