1d125cd846ac0b197f7171ade67ad69080e4ba66
florrosa <125082598+monica-l-p@users.noreply.github.com>
2024-04-08 12:10:38 +0200

    -

---
+++ project/my-app/src/app/leer-mensaje/leer-mensaje.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Mensaje } from '../mensaje';
+import { Destinatario } from '../destinatario';
+import { MensajeService } from "../mensaje.service";
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GerentesService } from '../gerentes.service';
+import { Gerente } from '../gerente';
+
+
+@Component({
+    selector: 'app-leer-mensaje',
+    templateUrl: './leer-mensaje.component.html',
+    styleUrls: ['./leer-mensaje.component.css']
+})
+
+export class LeerMensaje {
+    @Input() mensaje?: Mensaje;
+    @Output() gerenteEditado = new EventEmitter<Gerente>();
+    @Output() gerenteEliminado = new EventEmitter<number>();
+
+    constructor(private gerentesService: GerentesService, private modalService: NgbModal, public modal: NgbActiveModal) { }
+
+    eliminarGerente(): void {
+        this.gerenteEliminado.emit(this.mensaje?.getIdMensaje());
+        this.modal.close();
+    }
+}
