90fda0828a1ff05e7fe57895ea93d3fe848d40da
florrosa <125082598+monica-l-p@users.noreply.github.com>
2024-04-23 13:20:26 +0200

    Clase Centro terminada

--- project/backend/src/main/java/com/jpa/backend/entities/Centro.java
+++ project/backend/src/main/java/com/jpa/backend/entities/Centro.java
@@ -1,8 +1,17 @@
 package com.jpa.backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
 public class Centro {
     private String nombre;
     private String direccion;
+    @Id
+    @GeneratedValue
     private Integer idCentro;
 
     public String getNombre() {
@@ -27,5 +36,27 @@ public class Centro {
 
     public void setIdCentro(Integer idCentro) {
         this.idCentro = idCentro;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Centro centro = (Centro) o;
+        return Objects.equals(nombre, centro.nombre) && Objects.equals(direccion, centro.direccion) && Objects.equals(idCentro, centro.idCentro);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, direccion, idCentro);
+    }
+
+    @Override
+    public String toString() {
+        return "Centro{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", idCentro=" + idCentro +
+                '}';
     }
 }
