b3fe53e590a7a339b3dc2eaf3e2fba15d87ee4f4
florrosa <125082598+monica-l-p@users.noreply.github.com>
2024-04-23 13:13:21 +0200

    Clase Centro rellenada parcialmente

--- project/backend/src/main/java/com/jpa/backend/entities/Centro.java
+++ project/backend/src/main/java/com/jpa/backend/entities/Centro.java
@@ -1,4 +1,31 @@
 package com.jpa.backend.entities;
 
 public class Centro {
+    private String nombre;
+    private String direccion;
+    private Integer idCentro;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public Integer getIdCentro() {
+        return idCentro;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setIdCentro(Integer idCentro) {
+        this.idCentro = idCentro;
+    }
 }
