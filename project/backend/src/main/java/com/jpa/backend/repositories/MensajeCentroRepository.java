5a9803af90a21e3e737ce2861dd024ab4f97f68d
tessLuaDef <tessaluadef@uma.es>
2024-04-25 13:23:12 +0200

    No se si esta bien

--- project/backend/src/main/java/com/jpa/backend/repositories/MensajeCentroRepository.java
+++ project/backend/src/main/java/com/jpa/backend/repositories/MensajeCentroRepository.java
@@ -1,8 +1,30 @@
 package com.jpa.backend.repositories;
 
 import com.jpa.backend.entities.MensajeCentro;
+
+import jakarta.transaction.Transactional;
+
+import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 
 public interface MensajeCentroRepository extends JpaRepository<MensajeCentro, Long> {
-    MensajeCentro findById(long id);
+   
+    MensajeCentro findById(long idMensaje);
+
+
+    @Query("SELECT m FROM MensajeCentro m WHERE m.centro.id = :centroId")
+    List<MensajeCentro> findAllByCentroId(long centroId);
+
+    @Transactional
+    @Modifying
+    @Query("INSERT INTO MensajeCentro (texto, destinatario) VALUES (:texto, :gerenteId)")
+    void createNewMessage(String texto, long gerenteId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM MensajeCentro m WHERE m.id = :idMensaje")
+    void deleteById(long idMnesaje);
+
 }
