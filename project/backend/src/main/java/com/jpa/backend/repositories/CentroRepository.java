331ceca0322f0ae9af9036d1356ae1a63e268a27
florrosa <125082598+monica-l-p@users.noreply.github.com>
2024-04-25 13:17:30 +0200

    Metodo delete para CentroRepository

--- project/backend/src/main/java/com/jpa/backend/repositories/CentroRepository.java
+++ project/backend/src/main/java/com/jpa/backend/repositories/CentroRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CentroRepository extends JpaRepository<Centro, Long> {
     Centro findById(long id);
+    void deleteById(long id);
 }
